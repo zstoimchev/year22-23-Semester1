e27851b590529b8e078c8195fcf510344360515e
zstoimchev <zivkostoimcev1551@outlook.com>
2022-12-18 14:39:57 +0100

    finishing project1

--- Programiranje I EN 22-23/Homeworks/Project1/src/Main.java
+++ Programiranje I EN 22-23/Homeworks/Project1/src/Main.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Scanner;
-//import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
--- Programiranje I EN 22-23/_code/lab/gui/src/PopUp.java
+++ Programiranje I EN 22-23/_code/lab/gui/src/PopUp.java
@@ -1,16 +1,22 @@
 import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 
 public class PopUp {
-    String name = JOptionPane.showInputDialog(null, "Your name?", "Input", JOptionPane.QUESTION_MESSAGE);
-    String years = JOptionPane.showInputDialog(null, "How old are you?", "Input", JOptionPane.QUESTION_MESSAGE);
-    String years1 = JOptionPane.showInputDialog(null, "In which year of studying are you enrolled?", "Input", JOptionPane.QUESTION_MESSAGE);
+    public static void main(String[] args) {
 
-    int year = Integer.parseInt(years);
-    int year1 = Integer.parseInt(years1);
-    String message="Well, I know you! You are " + name +" and you are \n";
-    message+=year+" old. You go to FAMNIT and you are \n";
-    message+=year1+" , year of studying. \n Nice to meet you!";
+        String name = JOptionPane.showInputDialog(null, "Your name?", "Input", JOptionPane.QUESTION_MESSAGE);
+        String years = JOptionPane.showInputDialog(null, "How old are you?", "Input", JOptionPane.QUESTION_MESSAGE);
+        String years1 = JOptionPane.showInputDialog(null, "In which year of studying are you enrolled?", "Input", JOptionPane.QUESTION_MESSAGE);
 
-    JOptionPane.showMessageDialog(null, message, "output", JOptionPane.INFORMATION_MESSAGE);
-    System.exit(0);
+        int year = Integer.parseInt(years);
+        int year1 = Integer.parseInt(years1);
+        String message = "Well, I know you! You are " + name + " and you are \n";
+        message += year + " old. You go to FAMNIT and you are \n";
+        message += year1 + " , year of studying. \nNice to meet you!";
+
+        JOptionPane.showMessageDialog(null, message, "output", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
 }
