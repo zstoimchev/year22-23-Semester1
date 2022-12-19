0e2c4b451da5f92829f7ca787d5b63c812fe9dc4
zstoimchev <zivkostoimcev1551@outlook.com>
2022-12-19 08:31:19 +0100

    sorting files

---
+++ Computer Practicum I 22-23/_code/c/SampleMidterm2/task3-InemCrafting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int dPicaxes, dSwords, dBeads;
+	printf("Number of pickaxes: ");
+
+	printf("Number of pickaxes: ");
+
+	printf("Number of pickaxes: ");
+
+	
+	return 0;
+}
